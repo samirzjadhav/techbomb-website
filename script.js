61e1d2086971196e6df0f6ddca438a04ee2c69b6
Samir Jadhav <samirzjadhav@gmail.com>
2023-02-21 15:13:49 +0530

    fix: convert var to const

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Copy menu for mobile
 function copyMenu() {
   // Copy insane .dpt-cat to .departments
-  let dptCategory = document.querySelector(".dpt-cat");
-  let dptPlace = document.queryselector(".departments");
+  const dptCategory = document.querySelector(".dpt-cat");
+  const dptPlace = document.querySelector(".departments");
   dptPlace.innerHTML = dptCategory.innerHTML;
 
   // Copy inside nav to nav
-  let mainNav = document.querySelector(".header-nav nav");
-  let navPlace = document.querySelector(".off-canvas nav");
+  const mainNav = document.querySelector(".header-nav nav");
+  const navPlace = document.querySelector(".off-canvas nav");
   navPlace.innerHTML = mainNav.innerHTML;
 
   // Copy .header-top .wrapper to .thetop-nav
-  let topNav = document.querySelector(".header-top");
-  let topPlace = document.querySelector(".off-canvas .thetop-nav");
+  const topNav = document.querySelector(".header-top");
+  const topPlace = document.querySelector(".off-canvas .thetop-nav");
   topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
