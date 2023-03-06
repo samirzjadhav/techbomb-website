2dab89c20955eea62dfa5e981fd66f0d81156ae5
Samir Jadhav <samirzjadhav@gmail.com>
2023-03-06 15:31:51 +0530

    feat: added script for showsearch

--- script.js
+++ script.js
@@ -52,3 +52,15 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
+
+// Show search
+const searchButton = document.querySelector(".t-search");
+const tClose = document.querySelector(".search-close");
+const showClass = document.querySelector(".site");
+
+searchButton.addEventListener("click", function () {
+  showClass.classList.toggle("showsearch");
+});
+tClose.addEventListener("click", function () {
+  showClass.classList.remove("showsearch");
+});
