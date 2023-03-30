4b9283466d0d55d155e41ff5e3255bc03d099e8f
Samir Jadhav <samirzjadhav@gmail.com>
2023-03-30 19:12:34 +0530

    feat: added stock script

--- script.js
+++ script.js
@@ -99,3 +99,14 @@ var productBig = new Swiper(".big-image", {
     swiper: productThumb,
   },
 });
+
+// Stock Products bar with percentage
+const stocks = document.querySelectorAll(".products .stock");
+for (let x = 0; x < stocks.length; x++) {
+  let stock = stocks[x].dataset.stock;
+  let available = stocks[x].querySelector(".qty-available").innerHTML;
+  let sold = stocks[x].querySelector(".qty-sold").innerHTML;
+  let percent = (sold * 100) / stock;
+
+  stocks[x].querySelector(".available").style.width = percent + "%";
+}
